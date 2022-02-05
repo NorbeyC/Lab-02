af20dcf5dbff7432d32c18310bb537ee13a4f809
norbey.cardona@mail.escuelaing.edu.co <norbey.cardona@mail.escuelaing.edu.co>
2022-02-04 23:46:49 -0500

    entrega final

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,7 +1,33 @@
 package edu.eci.cvds.patterns.shapes;
 
+import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
+import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
+import edu.eci.cvds.patterns.shapes.concrete.Triangle;
+
 public class ShapeFactory {
-	public void create(String[] a) {
+
+	public static Shape create(RegularShapeType shape) {
+		switch(shape){
+			case "Triangle":
+				Triangle tri = new Shape();
+				return tri;
+				break;
+
+			case "Quadrilateral":
+				Quadrilateral qua = new Shape();
+				return qua;
+				break;
+			
+			case "Pentagon":
+				Pentagon pen = new Shape();
+				return pen;
+				break;
+
+			case "Hexagon":
+				Triangle hex = new Shape();
+				return hex;
+				break;
+		}
 		
 	}
 }
